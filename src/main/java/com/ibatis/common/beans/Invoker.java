72577e9f0de0b9fca0cf04232b9bd6982ae58ee6
hejin016@gmail.com <hejin016@gmail.com@a8f834c0-0a50-dfb0-f03c-27c8dfa61fd9>
2011-07-17 11:49:48 +0000

    prepare for shardbatis 2.0

--- src/main/java/com/ibatis/common/beans/Invoker.java
+++
@@ -1,8 +0,0 @@
-package com.ibatis.common.beans;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface Invoker {
-  String getName();
-  Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException;
-}
